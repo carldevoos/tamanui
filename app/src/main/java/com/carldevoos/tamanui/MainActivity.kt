b19e4b13388d4b6f5a985158dea6600643e0b469
carldevoos <carl.devoos@gmail.com>
2022-11-03 23:11:17 -0500

    working recyclerview

--- app/src/main/java/com/carldevoos/tamanui/MainActivity.kt
+++ app/src/main/java/com/carldevoos/tamanui/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.carldevoos.tamanui
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.carldevoos.tamanui.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
